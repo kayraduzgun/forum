b357938d68c6b8f0b7b093d18a7414bad8b198b3
kayraduzgun <abdurkadircalar@gmail.com>
2023-05-21 07:41:29 +0300

    forum

---
+++ setting/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    token: "MTEwNzIzNzM2NzQyNjg1NTAxMw.GvLKjY.2Ia_SnWCIgtKfIL7wqUzjhBw22EYynz2NZ9PY0", 
+    prefix: ".",
+    Footer: "BEDNEİ TR TEAM",
+    guildID: "1098201434723463198",
+    basvuruYt: "1109576689098424480",
+    yetkiRolleri: "1098645618106302595",
+    logKanalı: "1109576999145574561",
+    basvuruDurum: "1109576277628821584"
+}
+
---
+++ setting/sorular.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+    soru1: "İsim yaş?",
+    cevap1: "Burak 15",
+
+    soru2: "Sunucuda ne kadar aktifsiniz?",
+    cevap2: "13 saat",
+
+    soru3: "Sunucu İçin Neler Yapabilirsiniz?",
+    cevap3: "çekiliş, etkinilk vb.",
+
+    soru4: "Daha önce başka sunucuda yetkili oldun mu?",
+    cevap4: "5 sunucuda yetkili oldum",
+
+    soru5: "Neden yetkili olmak istiyorsun?",
+    cevap5: "işsizim"
+
+}
+
