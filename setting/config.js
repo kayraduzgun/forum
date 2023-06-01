8e42ed774503ea5b8ec46b87ca28b8c99501b157
kayraduzgun <abdurkadircalar@gmail.com>
2023-06-01 16:12:22 +0300

    deneme

--- setting/config.js
+++ setting/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    token: "MTEwNzIzNzM2NzQyNjg1NTAxMw.GFwXz8.B9HBeBTE-sTVQwaF03iLTnEBlwjDeMnBE1Mhio", 
+    token: "MTEwNzIzNzM2NzQyNjg1NTAxMw.GgVS6K.ynicHZgPzbamsNLbJRbIdCxlOss5EP8M_TxaOU", 
     prefix: ".",
     Footer: "BEDNEİ TR TEAM",
     guildID: "1098201434723463198",
-    basvuruYt: "1109576689098424480",
+    basvuruYt: "1090328401556029480",
     yetkiRolleri: "1098645618106302595",
-    logKanalı: "1109576999145574561",
-    basvuruDurum: "1109576277628821584"
+    logKanalı: "1113516054744924200",
+    basvuruDurum: "1113514477061689364"
 }
 
